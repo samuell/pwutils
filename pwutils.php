d0308de2b7171c545b5a84210290645d9dba5019
Samuel Lampa <samuel.lampa@gmail.com>
2014-02-10 14:37:41 +0100

    Added some functions corresponding to HTML tags

--- pwutils.php
+++ pwutils.php
@@ -4,6 +4,28 @@
 //
 //
 
+function tag( $name, $text, $attrs=array() ) {
+    $tag = "";
+    $tag .= '<' . $name;
+    foreach( $attrs as $attrName => $attrContent ) {
+        $tag .= ' ' . $attrName . '="' . $attrContent . '"';
+    }
+    $tag .= '>' . $text . '</' . $name . '>';
+    return $tag;
+}
+function a( $url, $title ) {
+    return tag( 'a', $title, array( 'href' => $url ));
+}
+function li( $text ) {
+    return tag( 'li', $text );
+}
+function h2( $text ) {
+    return tag( 'h2', $text );
+}
+function p( $text, $attrs ) {
+    return tag( 'p', $text, $attrs );
+}
+
 function formatDate( $date ) {
     if(is_int($date)) {
         // get date format from our 'date' field, for consistency
@@ -15,7 +37,7 @@ function formatDate( $date ) {
 function list_as_ul( $pages ) {
     echo '<ul>';
     foreach( $pages as $page ) {
-        echo '<li><a href="' . $page->url . '">' . $page->title . '</a></li>';
+        echo li( a( $page->url, $page->title ) );
     }
     echo '</ul>';
 }
@@ -23,10 +45,14 @@ function list_as_ul( $pages ) {
 function list_as_excerpts( $pages ) {
     echo '<div>';
     foreach( $pages as $page ) {
-        $createdUser = $page->createdUser;
-        echo '<h2><a href="' . $page->url . '">' . $page->title . '</a></h2>';
-        echo '<p style="font-size: 0.8em;">Posted on ' . formatDate( $page->created ) . ' by <a href="' . $createdUser->url . '">' . $createdUser->name . '</a></p>';
-        echo '<p>' . substr( $page->body, 0, 70 ) . '</p>';
+        $crUser = $page->createdUser;
+        echo h2( a( $page->url, $page->title ) );
+        $postedText = 'Posted on ' . 
+            formatDate( $page->created ) . 
+            ', by ' . 
+            a( $crUser->url, $crUser->name );
+        echo p( $postedText,  array('style' => 'font-size: 0.8em;' ) );
+        echo p( substr( $page->body, 0, 70 ) );
     }
     echo '</div>';
 }
