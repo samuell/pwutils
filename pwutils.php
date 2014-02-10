4fb3ba568e558628160e362cdebc840fc5ae2424
Samuel Lampa <samuel.lampa@gmail.com>
2014-02-10 14:07:45 +0100

    Output date in excerpts

--- pwutils.php
+++ pwutils.php
@@ -4,6 +4,14 @@
 //
 //
 
+function formatDate( $date ) {
+    if(is_int($date)) {
+        // get date format from our 'date' field, for consistency
+        $date = FieldtypeDatetime::formatDate($date, "%Y-%m-%d");
+    }   
+    return $date; 
+}
+
 function list_as_ul( $pages ) {
     echo '<ul>';
     foreach( $pages as $page ) {
@@ -16,6 +24,7 @@ function list_as_excerpts( $pages ) {
     echo '<div>';
     foreach( $pages as $page ) {
         echo '<h2><a href="' . $page->url . '">' . $page->title . '</a></h2>';
+        echo '<p style="font-size: 0.8em;">Posted on ' . formatDate( $page->created ) . '</p>';
         echo '<p>' . substr( $page->body, 0, 70 ) . '</p>';
     }
     echo '</div>';
