52a040429a3ce01e6e5a2076f0c29ce1238ecdb5
Samuel Lampa <samuel.lampa@gmail.com>
2014-02-10 13:52:37 +0100

    Two first listing functions

--- pwutils.php
+++ pwutils.php
@@ -3,3 +3,21 @@
 // More to come here ...
 //
 //
+
+function list_as_ul( $pages ) {
+    echo '<ul>';
+    foreach( $pages as $page ) {
+        echo '<li><a href="' . $page->url . '">' . $page->title . '</a></li>';
+    }
+    echo '</ul>';
+}
+
+function list_as_excerpts( $pages ) {
+    echo '<div>';
+    foreach( $pages as $page ) {
+        echo '<h2><a href="' . $page->url . '">' . $page->title . '</a></h2>';
+        echo '<p>' . substr( $page->body, 0, 70 ) . '</p>';
+    }
+    echo '</div>';
+}
+
